70ebb010c8c94847c07b4ab83a57ab3883fe076c
TomioDeCode <tomiodecode@gmail.com>
2024-09-24 03:53:13 +0000

    add: gitignore

--- models/klinik_dokter.py
+++ models/klinik_dokter.py
@@ -1,6 +1,29 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import UserError
+
 
 class KlinikDokter(models.Model):
-  _name = "klinik.dokter"
+    _name = "klinik.dokter"
 
-  name = fields.Many2one('res.partner', domain="[('klinik_member', '=', 'dokter')]", string="Nama Dokter")
+    name = fields.Many2one(
+        "res.partner", domain="[('klinik_member', '=', 'dokter')]", string="Dokter"
+    )
+    phone = fields.Char(string="Telepon", required=True)
+    email = fields.Char(string="Email", required=True)
+    alamat = fields.Text(string="Alamat", required=False)
+
+    @api.onchange("name")
+    def _onchange_name(self):
+        if self.name:
+            self.phone = self.name.phone
+            self.email = self.name.email
+            if (
+                self.name.street
+                or self.name.city
+                or self.name.state_id.name
+                or self.name.zip
+                or self.name.country_id.name
+            ):
+                self.alamat = f"{self.name.street or ''}, {self.name.city or ''}, {self.name.state_id.name or ''}, {self.name.zip or ''}, {self.name.country_id.name or ''}"
+            else:
+                self.alamat = False
--- models/klinik_ruangan.py
+++ models/klinik_ruangan.py
@@ -1,28 +1,24 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError
 
+
 class KlinikRuangan(models.Model):
-  _name = "klinik.ruangan"
-  _description = "Room for Pet Clinic"
-  
-  name = fields.Char(string="Appointment Reference", required=True, default="New")
-  is_available = fields.Boolean(string="Is Available", default=True)
-  unavailable_until = fields.Datetime(string="Unavailable Until")
+    _name = "klinik.ruangan"
+    _description = "Room for Pet Clinic"
 
-  def check_availability(self, appointment_date):
-    if not self.is_available or (self.unavailable_until and self.unavailable_until > appointment_date):
-      return False
-    return True
+    name = fields.Char(string="Appointment Reference", required=True, default="New")
+    is_available = fields.Boolean(string="Is Available", default=True)
+    unavailable_until = fields.Datetime(string="Unavailable Until")
 
-  def book_room(self, end_time):
-    self.write({
-      'is_available': False,
-      'unavailable_until': end_time
-    })
+    def check_availability(self, appointment_date):
+        if not self.is_available or (
+            self.unavailable_until and self.unavailable_until > appointment_date
+        ):
+            return False
+        return True
 
-  def free_room(self):
-    self.write({
-      'is_available': True,
-      'unavailable_until': False
-    })
-    
+    def book_room(self, end_time):
+        self.write({"is_available": False, "unavailable_until": end_time})
+
+    def free_room(self):
+        self.write({"is_available": True, "unavailable_until": False})
